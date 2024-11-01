f68ac41f5c7b4f336f9e69c12b17fca0ca60c96e
LcckyBoyy <myboxnumber17@gmail.com>
2024-11-01 15:22:39 +0700

    test lagi

--- GATES.API/Program.cs
+++ GATES.API/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -16,6 +15,7 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
