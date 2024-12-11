37284046e9332c99bb861c44de30a24fcae14a25
Lucky Boy <myboxnumber17@gmail.com>
2024-12-11 23:33:05 +0700

    Update GatesContext.cs

--- Back-End/GATES.DB/DB/GatesContext.cs
+++ Back-End/GATES.DB/DB/GatesContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace GATES.DB.DB;
@@ -32,8 +33,7 @@ public partial class GatesContext : DbContext
     public virtual DbSet<PSupplier> PSuppliers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LCCKY-BOY\\SQL24;User ID=sa;Password=sasa;Database=dbInventories;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["GATES"].ConnectionString);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
